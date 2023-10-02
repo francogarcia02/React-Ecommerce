0b49a1dee70248775ab930fca638aebd136a8fea
francogarcia02 <francoalbertogarcia2017@gmail.com>
2023-10-02 16:14:15 -0300

    primeros avances

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
-}
+};
+
+
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <nav className="Navbar">
+            <h1 className="Navbar__obj" >Ecommerce</h1>
+            <a className="Navbar__obj" href="" >Celulares</a>
+            <a className="Navbar__obj" href="" >Tablets</a>
+            <a className="Navbar__obj" href="" >Televisores</a>
+
+        </nav>
+    );
+};
+
+export default Navbar
