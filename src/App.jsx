e45c0ea0a1647921c35a52b9008be6dcd9e84437
francogarcia02 <francoalbertogarcia2017@gmail.com>
2023-10-05 21:27:34 -0300

    añadiendo json

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
+import dataContent from './components/dataContent/dataContent'
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar/>
+      <dataContent/>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
