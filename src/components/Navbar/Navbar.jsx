ce051dc1f666098c497cae25623432e6a85dfdc0
francogarcia02 <francoalbertogarcia2017@gmail.com>
2023-10-18 16:40:58 -0300

    iniciando el carrito

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,62 @@
 import './Navbar.css';
 import { useState } from 'react';
+import Cart from '../Cart/Cart'
 import DataContent from '../DataContent/DataContent'
 import Landing from '../Landing/Landing'
 
+
 const Navbar = () => {
 
+    const [landingOn, setlandingOn] = useState(true);
     const [menuOn, setmenuOn] = useState(false);
+    const [cartOn, setcartOn] = useState(false);
+
+    const [claseMenu, setclaseMenu] = useState("Navbar__item");
 
     const abrirMenu = () =>{
+        setlandingOn(false)
+        setcartOn(false)
         setmenuOn(true)
+        setclaseMenu("Navbar__item-use")
+    }
+
+    const abrirCarrito = () =>{
+        setlandingOn(false)
+        setmenuOn(false)
+        setcartOn(true)
+        setclaseMenu("Navbar__item")
     }
 
     const abrirLanding = () =>{
         setmenuOn(false)
+        setcartOn(false)
+        setlandingOn(true)
+        setclaseMenu("Navbar__item")
     }
 
     return(
-        <header className="stopper">
-            <nav className="Navbar">
-                <button onClick={abrirLanding} className="Navbar__landing" >Ecommerce</button>
-                <button onClick={abrirMenu} className="Navbar__menu" >Menu</button>
-                <a className="Navbar__obj Navbar__obj_login" href="" >Iniciar sesion</a>
-                <a className="Navbar__obj Navbar__obj_register" href="" >Registrarse</a>
-            </nav>
-            {menuOn === true &&
-                <DataContent></DataContent>
-            }
-            {menuOn === false &&
-                <Landing></Landing>
-            }
-        </header>
+        <div>
+            <header className="stopper">
+                <nav className="Navbar">
+                    <button onClick={abrirLanding} className="Navbar__landing" >Ecommerce</button>
+                    <button onClick={abrirMenu} className={claseMenu} >Menu</button>
+                    <button onClick={abrirCarrito} className="Navbar__item" >Carrito</button>
+                    <a className="Navbar__obj Navbar__obj_login" href="" >Iniciar sesion</a>
+                    <a className="Navbar__obj Navbar__obj_register" href="" >Registrarse</a>
+                </nav>
+            </header>
+            <main>
+                {menuOn === true &&
+                    <DataContent></DataContent>
+                }
+                {landingOn === true &&
+                    <Landing></Landing>
+                }
+                {cartOn === true &&
+                    <Cart></Cart>
+                }
+            </main>
+        </div>
     );
 };
 
